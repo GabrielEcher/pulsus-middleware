e05f7681701cba6678e12cdca1a936156a51708a
Gabriel Ecker <gabriel@diamaju.local>
2025-07-15 09:31:59 -0300

    alterada para base prod url banco

--- database/oracle_connection.py
+++ database/oracle_connection.py
@@ -7,6 +7,6 @@ load_dotenv()
 user = os.getenv("DB_USER")
 pwd = os.getenv("DB_PASSWORD")
 
-oracle_engine = create_engine(f"oracle+oracledb://{user}:{pwd}@10.0.0.21/dbtest2")
+oracle_engine = create_engine(f"oracle+oracledb://{user}:{pwd}@10.0.0.22/orcl2")
 
 Session = sessionmaker(bind=oracle_engine)
